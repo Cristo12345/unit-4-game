72cbde1e97dc924d41e52e7851ef292b0408241e
Christian Suleiman <cisuleiman@gmail.com>
2019-06-06 14:52:12 -0400

    game is working but hard coded for the crystals. Should make each crystal an object with its own attributes to be called for a general click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
+//vars to score wins and losses
+var wins = 0;
+var losses = 0;
+
 //variable to store random target number
 var targetScore = 0;
 
 //User's score so far, must be reset to 0 after each game
-var totalScore = 0;
+var userScore = 0;
 
 //vars to store the values of each crystal
 var blueValue;
@@ -22,17 +26,59 @@ function startGame() {
 
 
     //reset user's score
-    totalScore = 0;
+    userScore = 0;
+
+    //update wins and loss display
+    $("#wins-text").html(wins);
+    $("#losses-text").html(losses);
 
     //assign random values to each crystal - this method leaves the possibility of different crystals having the same value, however
     blueValue = Math.floor(Math.random() * (13 - 1) + 1);
     redValue = Math.floor(Math.random() * (13 - 1) + 1);
     greenValue = Math.floor(Math.random() * (13 - 1) + 1);
     yellowValue = Math.floor(Math.random() * (13 - 1) + 1);
+};
 
-    // console.log("blueValue: ", blueValue);
-    // console.log("redValue: ", redValue);
-    // console.log("greeValue: ", greenValue);
-    // console.log("yellowValue: ", yellowValue);
+//function to check if user has hit target score, should be checked after each click on a crystal
+//if userScore == targetScore --> user has won, increment wins, reset game
+//if userScore > targetScore --> user has lost, increment losses, reset game
+//else, keep playing
 
-}
+function checkWin() {
+    if (userScore == targetScore) {
+        wins++;
+        startGame();
+        alert("You won!");
+    }
+
+    else if (userScore > targetScore) {
+        losses++;
+        startGame();
+        alert("You lost!");
+    }
+};
+
+
+$("#blueCrystal").bind("click", function() {
+    userScore += blueValue;
+    checkWin();
+    $("#total-score-text").html(userScore);
+});
+
+$("#redCrystal").bind("click", function() {
+    userScore += redValue;
+    checkWin();
+    $("#total-score-text").html(userScore);
+});
+
+$("#greenCrystal").bind("click", function() {
+    userScore += greenValue;
+    checkWin();
+    $("#total-score-text").html(userScore);
+});
+
+$("#yellowCrystal").bind("click", function() {
+    userScore += yellowValue;
+    checkWin();
+    $("#total-score-text").html(userScore);
+});
